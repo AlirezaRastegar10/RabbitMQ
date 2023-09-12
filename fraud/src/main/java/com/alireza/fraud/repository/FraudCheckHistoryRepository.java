74cc7b193af850aaabdbdf4ae35c27c167471343
phylix <alirezarastegar117390@gmail.com>
2023-09-12 19:58:04 +0330

    create FraudCheckHistoryRepository as data access layer in fraud microservice

---
+++ fraud/src/main/java/com/alireza/fraud/repository/FraudCheckHistoryRepository.java
@@ -0,0 +1,10 @@
+package com.alireza.fraud.repository;
+
+
+import com.alireza.fraud.entity.FraudCheckHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Long> {
+}

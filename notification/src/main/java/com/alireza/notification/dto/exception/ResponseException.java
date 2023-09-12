21f2067049c727d598794cb2963b10802206ced2
phylix <alirezarastegar117390@gmail.com>
2023-09-12 20:05:08 +0330

    create ResponseException dto to send exception information

---
+++ notification/src/main/java/com/alireza/notification/dto/exception/ResponseException.java
@@ -0,0 +1,21 @@
+package com.alireza.notification.dto.exception;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ResponseException {
+
+    Timestamp timestamp;
+    Integer status;
+    String error;
+    String message;
+    String path;
+}
